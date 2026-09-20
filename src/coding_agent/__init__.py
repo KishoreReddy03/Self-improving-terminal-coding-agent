0a7a654193c63fcd0bb1d4d91c3cc2382b46e1a2
Kishore Reddy <kishorereddy5468@gmail.com>
2026-09-20 22:32:52 +0530

    feat: export Tool abstraction in package root __init__.py

--- src/coding_agent/__init__.py
+++ src/coding_agent/__init__.py
@@ -2,12 +2,14 @@
 
 from coding_agent.config import Config, load_config_from_env
 from coding_agent.models import AgentConfig, ToolCall
+from coding_agent.tools import Tool
 
 __version__ = "0.1.0"
 
 __all__ = [
     "AgentConfig",
     "Config",
+    "Tool",
     "ToolCall",
     "load_config_from_env",
     "__version__",
