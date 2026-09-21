a66f8fed72789007f0069a8c59b7ca61cd4fc89a
Kishore Reddy <kishorereddy5468@gmail.com>
2026-09-21 22:51:56 +0530

    feat: export WriteFileTool and EditFileTool in package root __init__.py

--- src/coding_agent/__init__.py
+++ src/coding_agent/__init__.py
@@ -2,16 +2,18 @@
 
 from coding_agent.config import Config, load_config_from_env
 from coding_agent.models import AgentConfig, ToolCall
-from coding_agent.tools import ReadFileTool, Tool
+from coding_agent.tools import EditFileTool, ReadFileTool, Tool, WriteFileTool
 
 __version__ = "0.1.0"
 
 __all__ = [
     "AgentConfig",
     "Config",
+    "EditFileTool",
     "ReadFileTool",
     "Tool",
     "ToolCall",
+    "WriteFileTool",
     "load_config_from_env",
     "__version__",
 ]
