58d35ec04ed74830124a78eb3500d93052815749
Kishore Reddy <kishorereddy5468@gmail.com>
2026-09-21 22:45:12 +0530

    feat: export ReadFileTool in package root __init__.py

--- src/coding_agent/__init__.py
+++ src/coding_agent/__init__.py
@@ -2,13 +2,14 @@
 
 from coding_agent.config import Config, load_config_from_env
 from coding_agent.models import AgentConfig, ToolCall
-from coding_agent.tools import Tool
+from coding_agent.tools import ReadFileTool, Tool
 
 __version__ = "0.1.0"
 
 __all__ = [
     "AgentConfig",
     "Config",
+    "ReadFileTool",
     "Tool",
     "ToolCall",
     "load_config_from_env",
