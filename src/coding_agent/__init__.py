558c329ab6212a4c411dea2131f98d138a394c51
Kishore Reddy <kishorereddy5468@gmail.com>
2026-09-25 22:42:00 +0530

    chore: export ResponseParser and StreamToolCallAccumulator in package init

--- src/coding_agent/__init__.py
+++ src/coding_agent/__init__.py
@@ -3,6 +3,7 @@
 from coding_agent.client import LLMClient
 from coding_agent.config import Config, load_config_from_env
 from coding_agent.models import AgentConfig, ModelResponse, ToolCall
+from coding_agent.parser import ResponseParser, StreamToolCallAccumulator
 from coding_agent.registry import ToolRegistry
 from coding_agent.tools import EditFileTool, ReadFileTool, ShellCommandTool, Tool, WriteFileTool
 
@@ -15,7 +16,9 @@ __all__ = [
     "LLMClient",
     "ModelResponse",
     "ReadFileTool",
+    "ResponseParser",
     "ShellCommandTool",
+    "StreamToolCallAccumulator",
     "Tool",
     "ToolCall",
     "ToolRegistry",
